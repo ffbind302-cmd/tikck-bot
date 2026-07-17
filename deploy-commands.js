b82bf023b474943f6854c39f9fd9961f69f09642
ffbind302-cmd <ffbind302@gmail.com>
2026-07-17 11:18:32 -0700

    dfgfdgfdgdf

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,32 +1,35 @@
-const { REST } = require('@discordjs/rest'); 
+const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const fs = require('fs');
-const {token, clientID} =  require('./config.json'); 
+const { token, clientID } = require('./config.json');
 
-exports.DeployCommands = async () => { 
+async function DeployCommands() {
 
-    const commands = [];  
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); 
-    
+    const commands = [];
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
     for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
         commands.push(command.data.toJSON());
     }
 
+    const rest = new REST({ version: '10' }).setToken(token);
 
-    const rest = new REST({ version: '10' }).setToken(token); 
-    
-    console.log('[DEPLOY] Deploying commands...'.yellow);
-    (async () => {
-        try {
-            console.log('[DEPLOY] Started refreshing application (/) commands.'.blue);
-            await rest.put(
-                Routes.applicationCommands(clientID),
-                { body: commands },
-            );
-            console.log('[DEPLOY] Successfully reloaded application (/) commands.'.green);
-        } catch (error) {
-            console.error(`[DEPLOY] Error while refreshing application (/) commands: ${error}`.red);
-        }
-    })();
+    try {
+        console.log("Deploying commands...");
+        await rest.put(
+            Routes.applicationCommands(clientID),
+            { body: commands }
+        );
+        console.log("✅ Commands deployed!");
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = { DeployCommands };
+
+// Agar file direct run karo (node deploy-commands.js)
+if (require.main === module) {
+    DeployCommands();
 }
